cf2cac8b71eabd98e4d10af6ef91276956f9dce7
Felipe Sant Ana <felipe.santana1995@hotmail.com>
2024-06-10 13:19:19 -0300

    Projeto para teste de shift da lista

---
+++ src/implementation/SupermarketArray.java
@@ -0,0 +1,18 @@
+package implementation;
+
+public class SupermarketArray implements Supermarket{
+    @Override
+    public void add(String item) {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+}
